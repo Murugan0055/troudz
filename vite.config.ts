7b483ada4537f7f084166cd5ceb5bc3c67b40036
murugan <muruganvenkatesan55@gmail.com>
2025-02-03 20:24:14 +0530

    Server setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  server: {
+    port: 80, // Set the port to 80
+    host: '0.0.0.0', // Allow access from external IP addresses
+    allowedHosts: true  // Allows any host
+  },
   plugins: [react()],
   resolve: {
     alias: {
