cd00d5af3dbbc734a3501ba1a18d00d12a732f20
murugan <muruganvenkatesan55@gmail.com>
2025-03-06 19:21:32 +0530

    test 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
 import path from "path"
 import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
-import fs from 'fs'
+// import fs from 'fs'
 
 export default defineConfig({
   server: {
-    port: 443, // Set the port to 80
-    host: '0.0.0.0', // Allow access from external IP addresses
-    allowedHosts: true, // Allows any host
-    https: {
-      key: fs.readFileSync('/etc/letsencrypt/live/troudz.com/privkey.pem'),
-      cert: fs.readFileSync('/etc/letsencrypt/live/troudz.com/fullchain.pem')
-    }
+    // port: 443, // Set the port to 80
+    // host: '0.0.0.0', // Allow access from external IP addresses
+    // allowedHosts: true, // Allows any host
+    // https: {
+    //   key: fs.readFileSync('/etc/letsencrypt/live/troudz.com/privkey.pem'),
+    //   cert: fs.readFileSync('/etc/letsencrypt/live/troudz.com/fullchain.pem')
+    // }
   },
   plugins: [react()],
   resolve: {
