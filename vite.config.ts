a7cb45c29d9c591819589bff50734f9e6a21f610
murugan <muruganvenkatesan55@gmail.com>
2025-03-06 19:22:26 +0530

    dev

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   server: {
-    // port: 443, // Set the port to 80
-    // host: '0.0.0.0', // Allow access from external IP addresses
-    // allowedHosts: true, // Allows any host
+    port: 443, // Set the port to 80
+    host: '0.0.0.0', // Allow access from external IP addresses
+    allowedHosts: true, // Allows any host
     // https: {
     //   key: fs.readFileSync('/etc/letsencrypt/live/troudz.com/privkey.pem'),
     //   cert: fs.readFileSync('/etc/letsencrypt/live/troudz.com/fullchain.pem')
